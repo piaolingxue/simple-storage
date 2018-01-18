612c03ffc5352bf912f28af6b33b88a22956e414
李斌 <libin1@u51.com>
2018-01-18 17:12:57 +0800

    init commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.17;
+
+contract SimpleStorage {
+  uint myVariable;
+
+  event Odd();
+  event Even();
+
+  function set(uint x) public {
+    myVariable = x;
+    if (x % 2 == 0) {
+      Even();
+    } else {
+      Odd();
+    }
+  }
+
+  function get() constant public returns (uint){
+    return myVariable;
+  }
+}
